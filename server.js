4d921e6e8564441d5dc5f61be2b06cabaf458117
DavideCicoria <cicoriadavide@itis-molinari.eu>
2025-01-29 19:44:09 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const path = require('path');
+app.use("/", express.static(path.join(__dirname, "public")));
+
+const todos = [];
+
+app.post("/todo/add", (req, res) => {
+   const todo = req.body.todo;
+   todo.id = "" + new Date().getTime();
+   todos.push(todo);
+   res.json({result: "Ok"});
+});
+
+app.get("/todo", (req, res) => {
+   res.json({todos: todos});
+});
+
+const server = http.createServer(app);
+server.listen(80, () => {
+    console.log("- server running");
+});
